eb99a6a7f7085ae8ff5be485040e724a78832e30
Djole01 <djordje_obradovic33@mm.st>
2020-04-21 13:38:10 +0300

    .

--- infrared remote/IRemote.ino
+++ infrared remote/IRemote.ino
@@ -1,9 +1,6 @@
 /*
  * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
  * An IR detector/demodulator must be connected to the input RECV_PIN.
- * Version 0.1 July, 2009
- * Copyright 2009 Ken Shirriff
- * http://arcfn.com
  */
 
 #include <IRremote.h>
